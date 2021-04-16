4c357ebcc9d4d07716be65a3bd2c3bc58e98e336
MissGod1 <missgodbaby@gmail.com>
2021-04-16 14:22:27 +0800

    修改日志文件

--- common/log/logger.go
+++ common/log/logger.go
@@ -7,7 +7,7 @@ import (
 )
 
 func NewLogger() *log.Logger {
-	logFile, err := os.OpenFile("WARNING.log", os.O_CREATE|os.O_WRONLY|os.O_APPEND, 0666)
+	logFile, err := os.OpenFile("pproxy-cli.log", os.O_CREATE|os.O_WRONLY|os.O_APPEND, 0666)
 	if err != nil {
 		log.Fatalln("打开日志文件失败:", err)
 	}
